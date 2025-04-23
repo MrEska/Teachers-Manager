020dfd27952dd8abb02deace9250f78e6aff60fa
Kamil <kamil.siarkowicz@op.pl>
2025-04-23 22:19:57 +0200

    Delete CompareSalary.java

--- CompareSalary.java
+++
@@ -1,7 +0,0 @@
-package pau.pau_zad3;
-
-import java.util.Comparator;
-
-public abstract class CompareSalary implements Comparator<Teacher> {
-    public abstract int compare(Teacher t1, Teacher t2);
-}
